b44a0411d97a5cbf4d5a4ab2eb92a56b74f360f3
Dimas Gabriel <dimas@dimasgabriel.net>
2018-10-11 10:52:03 -0300

    Testing UserModel

--- src/models/UserModel.ts
+++ src/models/UserModel.ts
@@ -1,73 +1,74 @@
-import { Document, Schema, Model, model} from "mongoose";
-import * as bcrypt from "bcrypt";
-import * as uniqueValidator from "mongoose-unique-validator";
+import {Document, Schema, Model, model} from 'mongoose';
+import * as bcrypt from 'bcrypt';
+import * as uniqueValidator from 'mongoose-unique-validator';
 
 const SALT_WORK_FACTOR = 10;
 
 export interface IUser {
-    name: String,
-    email: String,
-    gender?: String,
-    password: String
+  name: String;
+  email: String;
+  gender?: String;
+  password: String;
 }
 
 export interface IUserModel extends IUser, Document {
-    comparePassword(candidatePassword: String) : Promise<boolean>
+  comparePassword(candidatePassword: String): Promise<boolean>;
 }
 
 const UserSchema = new Schema({
-    name: {
-        type: String,
-        required: 'Enter your name'
-    },
+  name: {
+    type: String,
+    required: 'Enter your name'
+  },
 
-    email: {
-        type: String,
-        required: 'Enter your email',
-        unique: true
-    },
+  email: {
+    type: String,
+    required: 'Enter your email',
+    unique: true
+  },
 
-    gender: {
-        type: String,
-        required: false,
-        enum: ["male", "female"]
-    },
+  gender: {
+    type: String,
+    required: false,
+    enum: ['male', 'female']
+  },
 
-    password: {
-        type: String,
-        default: null,
-        minlength: [6, 'Your password must have at least 6 characters']
-    }
-})
+  password: {
+    type: String,
+    default: null,
+    minlength: [6, 'Your password must have at least 6 characters']
+  }
+});
 
 // Password comparsion method
-UserSchema.methods.comparePassword = async function(candidatePassword: string): Promise<boolean> {
-    return bcrypt.compare(candidatePassword, this.password);
-}
+UserSchema.methods.comparePassword = function(candidatePassword: string): Promise<boolean> {
+  console.log(`Comparing passwords: ${this.password} and ${candidatePassword}`);
+  return bcrypt.compare(candidatePassword, this.password);
+};
 
 // Pre Save Hook to hash the password
 UserSchema.pre('save', async function(next) {
-    const user = this;
+  const user = this;
 
-    // only hash the password if it has been modified (or is new)
-    if (!user.isModified('password')) return next();
+  // only hash the password if it has been modified (or is new)
+  if (!user.isModified('password')) return next();
 
-    // hash the new password and update
-    const password: string = user.get('password');
-    const hash = await bcrypt.hash(password, SALT_WORK_FACTOR);
-    user.set('password', hash);
+  // hash the new password and update
+  const password: string = user.get('password');
+  const hash = await bcrypt.hash(password, SALT_WORK_FACTOR);
+  user.set('password', hash);
 
-    next();
+  next();
 });
 
 UserSchema.set('toJSON', {
-    transform: function(doc, ret, opt) {
-        delete ret['password']
-        delete ret['email']
-        return ret
-    }
-})
+  transform: function(doc, ret, opt) {
+    delete ret['password'];
+    delete ret['email'];
+    return ret;
+  }
+});
 
-UserSchema.plugin(uniqueValidator, {message : "We found an user with the same {PATH}"});
+UserSchema.plugin(uniqueValidator, {message: 'We found an user with the same {PATH}'});
 
-export const User: Model<IUserModel> = model<IUserModel>("User", UserSchema);
+export const User: Model<IUserModel> = model<IUserModel>('User', UserSchema);
--- test/routes/AuthRouter.test.ts
+++ test/routes/AuthRouter.test.ts
@@ -1,7 +1,7 @@
 process.env.NODE_ENV = 'test';
 
-import { User } from './../../src/models/UserModel';
-import { application as app } from './../../src/server';
+import {User} from './../../src/models/UserModel';
+import {application as app} from './../../src/server';
 const chaiHttp = require('chai-http');
 import * as chai from 'chai';
 
@@ -9,73 +9,75 @@ const assert = chai.assert;
 chai.use(chaiHttp);
 
 describe('AuthRouter', () => {
-    // Empty Users before each test
-    before((done) => {
-            User.deleteMany({}, () => {
-            // Creates a test user
-            const userJSON = {
-                "email" : "dimas@test.com",
-                "password" : "123456",
-                "name" : "Dimas Gabriel",
-                "gender" : "male"
-            }; 
+  // Empty Users before each test
+  before((done) => {
+    User.deleteMany({}, () => {
+      // Creates a test user
+      const userJSON = {
+        email: 'dimas@test.com',
+        password: '123456',
+        name: 'Dimas Gabriel',
+        gender: 'male'
+      };
 
-            const user = new User(userJSON);
-            user.save().then(_ => {
-                done();
-            });
+      const user = new User(userJSON);
+      user.save().then((_) => {
+        done();
+      });
+    });
+  });
+
+  describe('POST /auth', () => {
+    // Auth JSON post body
+    const authJSON = {
+      email: 'dimas@test.com',
+      password: '123456'
+    };
+
+    it('it should authenticate a valid user and return an access token and the user information', (done) => {
+      chai
+        .request(app)
+        .post('/auth')
+        .send(authJSON)
+        .end((err, res) => {
+          console.log('Error: ' + err);
+          const data = res.body.data;
+
+          assert.equal(res.status, 200, 'Wrong status code: ' + res.status);
+          assert.notExists(err, 'We have an error: ' + err);
+
+          assert.typeOf(data.access_token, 'string', 'Response without access_token field');
+          assert.typeOf(data.refresh_token, 'string', 'Response without refresh_token field');
+          assert.exists(data.user, 'Response without user field');
+          assert.notExists(data.user.email, 'Response with user email included');
+          assert.notExists(data.user.password, 'Response with user password included');
+
+          done();
         });
     });
+  });
 
-    describe('POST /auth', () => {
-        // Auth JSON post body
-        const authJSON = {
-            "email" : "dimas@test.com",
-            "password" : "123456"
-        };
+  describe('POST /auth', () => {
+    // Auth JSON post body
+    const authJSON = {
+      email: 'dimas@test.com',
+      password: '12356'
+    };
 
-        it('it should authenticate a valid user and return an access token and the user information', (done) => {
-            chai.request(app)
-                .post('/auth')
-                .send(authJSON)
-                .end((err, res) => {
+    it('it should not authenticate an invalid email/password combinator and return the correct error code', (done) => {
+      chai
+        .request(app)
+        .post('/auth')
+        .send(authJSON)
+        .end((err, res) => {
+          const data = res.body.data;
 
-                    const data = res.body.data;
+          assert.equal(res.status, 401, 'Wrong status code');
+          assert.equal(res.body.code, 1002, 'Wrong error code');
+          assert.isString(res.body.message, 'No error message');
 
-                    assert.equal(res.status, 200, "Wrong status code: " + res.status);
-                    assert.notExists(err, "We have an error: " + err);
-                    
-                    assert.typeOf(data.access_token, 'string', "Response without access_token field");
-                    assert.typeOf(data.refresh_token, 'string', "Response without refresh_token field");
-                    assert.exists(data.user, "Response without user field");
-                    assert.notExists(data.user.email, 'Response with user email included');
-                    assert.notExists(data.user.password, 'Response with user password included');
-
-                    done();
-                });
-        })
+          done();
+        });
     });
-
-    describe('POST /auth', () => {
-        // Auth JSON post body
-        const authJSON = {
-            "email" : "dimas@test.com",
-            "password" : "12356"
-        };
-
-        it('it should not authenticate an invalid email/password combinator and return the correct error code', (done) => {
-            chai.request(app)
-                .post('/auth')
-                .send(authJSON)
-                .end((err, res) => {
-                    const data = res.body.data;
-
-                    assert.equal(res.status, 401, 'Wrong status code');
-                    assert.equal(res.body.code, 1002, 'Wrong error code');
-                    assert.isString(res.body.message, 'No error message');
-
-                    done();
-                });
-        })
-    });
+  });
 });
