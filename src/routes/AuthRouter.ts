4c1a5dfe546ab2d7b78eece3c3d8ec04d3e84170
Dimas Gabriel <dimas@dimasgabriel.net>
2018-10-11 12:25:59 -0300

    Bug fix

--- src/routes/AuthRouter.ts
+++ src/routes/AuthRouter.ts
@@ -1,73 +1,85 @@
-import { APIResponse } from './../models/APIResponse';
-import { APIError, APIErrorCodes } from './../models/APIError';
+import {APIResponse} from './../models/APIResponse';
+import {APIError, APIErrorCodes} from './../models/APIError';
 import {User} from './../models/UserModel';
-import {Router, Request, Response, Application} from "express";
-import { check, validationResult } from 'express-validator/check';
+import {Router, Request, Response, Application} from 'express';
+import {check, validationResult} from 'express-validator/check';
 import * as randomstring from 'randomstring';
 import * as jwt from 'jsonwebtoken';
 
 export class AuthRouter {
-    private router: Router
+   private router: Router;
 
-    private authValidations = [
-        check('email').isEmail(),
-        check('password').not().isEmpty()
-    ]
+   private authValidations = [
+      check('email').isEmail(),
+      check('password')
+         .not()
+         .isEmpty()
+   ];
 
-    constructor(app: Application) {
-        this.router = Router()
-        this.config();
-        
-        app.use('/auth', this.router);
-    }
+   constructor(app: Application) {
+      this.router = Router();
+      this.config();
 
-    private config() {
-        this.router.post('/', this.authValidations, this.authenticate);
-    }
+      app.use('/auth', this.router);
+   }
 
-    private async authenticate(req: Request, res: Response, next) {
-        if (!validationResult(req).isEmpty()) {
-            const apiError = new APIError(res.__('auth_wrong_request_message'), 400, 400)
+   private config() {
+      this.router.post('/', this.authValidations, this.authenticate);
+   }
+
+   private async authenticate(req: Request, res: Response, next) {
+      if (!validationResult(req).isEmpty()) {
+         const apiError = new APIError(res.__('auth_wrong_request_message'), 400, 400);
+         return next(apiError);
+      }
+
+      // Grant that an user with this email exists
+      const count = await User.count({email: req.body.email});
+      if (count == 0) {
+         const apiError = new APIError(
+            res.__('auth_failed_message'),
+            APIErrorCodes.AUTH_FAILED,
+            401
+         );
+         return next(apiError);
+      }
+
+      //Fetch the user
+      try {
+         const user = await User.findOne({email: req.body.email});
+         const pass = await user.comparePassword(req.body.password);
+
+         // Invalid password
+         if (!pass) {
+            const apiError = new APIError(
+               res.__('auth_failed_message'),
+               APIErrorCodes.AUTH_FAILED,
+               401
+            );
             return next(apiError);
-        }
+         }
 
-        // Grant that an user with this email exists
-        const count = await User.count({"email" : req.body.email});
-        if (count == 0) {
-            const apiError = new APIError(res.__('auth_failed_message'), APIErrorCodes.AUTH_FAILED, 401)
-            return next(apiError);
-        }
+         const refreshToken = randomstring.generate({length: 100, charset: 'alphabetic'});
+         jwt.sign(
+            {user_id: user._id},
+            process.env.JWT_SECRET,
+            {expiresIn: process.env.JWT_EXPIRATION_TIME},
+            function(err, token) {
+               if (err) {
+                  return next(APIError.unknown());
+               }
 
-        //Fetch the user
-        try { 
-            const user = await User.findOne({"email" : req.body.email});
-            const pass = await user.comparePassword(req.body.password);
+               const data = {
+                  access_token: token,
+                  refresh_token: refreshToken,
+                  user: user
+               };
 
-            // Invalid password
-            if (!pass) {
-                const apiError = new APIError(res.__('auth_failed_message'), APIErrorCodes.AUTH_FAILED, 401)
-                return next(apiError);
+               res.status(200).json(new APIResponse(true, 200, data));
             }
-
-            const refreshToken = randomstring.generate({length: 100, charset: 'alphabetic'});
-            jwt.sign({'user_id' : user._id}, process.env.JWT_SECRET, {expiresIn: process.env.JWT_EXPIRATION_TIME}, function(err, token) {
-                if (err) {
-                    return next(APIError.unknown());
-                }
-
-                const data = {
-                    access_token: token,
-                    refresh_token: refreshToken,
-                    user: user
-                };
-                
-                res.status(200).json(new APIResponse(true, 200, data));
-            })
-
-        }catch (e) {
-            return next(APIError.unknown());
-        }
-        
-
-    }
+         );
+      } catch (e) {
+         return next(APIError.unknown());
+      }
+   }
 }
--- test/routes/AuthRouter.test.ts
+++ test/routes/AuthRouter.test.ts
@@ -9,71 +9,71 @@ const assert = chai.assert;
 chai.use(chaiHttp);
 
 describe('AuthRouter', () => {
-  // Empty Users before each test
-  before((done) => {
-    User.deleteMany({}, () => {
-      // Creates a test user
-      const userJSON = {
-        email: 'dimas@test.com',
-        password: '123456',
-        name: 'Dimas Gabriel',
-        gender: 'male'
+   // Empty Users before each test
+   before((done) => {
+      User.deleteMany({}, () => {
+         // Creates a test user
+         const userJSON = {
+            email: 'dimas@test.com',
+            password: '123456',
+            name: 'Dimas Gabriel',
+            gender: 'male'
+         };
+
+         const user = new User(userJSON);
+         user.save().then((_) => {
+            done();
+         });
+      });
+   });
+
+   describe('POST /auth', () => {
+      // Auth JSON post body
+      const authJSON = {
+         email: 'dimas@test.com',
+         password: '123456'
       };
 
-      const user = new User(userJSON);
-      user.save().then((_) => {
-        done();
+      it('it should authenticate a valid user', (done) => {
+         chai
+            .request(app)
+            .post('/auth')
+            .send(authJSON)
+            .end((err, res) => {
+               const data = res.body.data;
+
+               assert.equal(res.status, 200, 'Wrong status code: ' + res.status);
+               assert.notExists(err, 'We have an error: ' + err);
+
+               assert.typeOf(data.access_token, 'string', 'Response without access_token field');
+               assert.typeOf(data.refresh_token, 'string', 'Response without refresh_token field');
+               assert.exists(data.user, 'Response without user field');
+               assert.notExists(data.user.email, 'Response with user email included');
+               assert.notExists(data.user.password, 'Response with user password included');
+
+               done();
+            });
       });
-    });
-  });
 
-  describe('POST /auth', () => {
-    // Auth JSON post body
-    const authJSON = {
-      email: 'dimas@test.com',
-      password: '123456'
-    };
+      const worngAuthJSON = {
+         email: 'dimas@test.com',
+         password: '12356'
+      };
 
-    it('it should authenticate a valid user', (done) => {
-      chai
-        .request(app)
-        .post('/auth')
-        .send(authJSON)
-        .end((err, res) => {
-          const data = res.body.data;
+      it('it should not authenticate an invalid email/password combination', (done) => {
+         chai
+            .request(app)
+            .post('/auth')
+            .send(worngAuthJSON)
+            .end((err, res) => {
+               const data = res.body.data;
 
-          assert.equal(res.status, 200, 'Wrong status code: ' + res.status);
-          assert.notExists(err, 'We have an error: ' + err);
+               assert.equal(res.status, 401, 'Wrong status code');
+               assert.equal(res.body.code, 1002, 'Wrong error code');
+               assert.isString(res.body.message, 'No error message');
 
-          assert.typeOf(data.access_token, 'string', 'Response without access_token field');
-          assert.typeOf(data.refresh_token, 'string', 'Response without refresh_token field');
-          assert.exists(data.user, 'Response without user field');
-          assert.notExists(data.user.email, 'Response with user email included');
-          assert.notExists(data.user.password, 'Response with user password included');
-
-          done();
-        });
-    });
-
-    const worngAuthJSON = {
-      email: 'dimas@test.com',
-      password: '12356'
-    };
-
-    it('it should not authenticate an invalid email/password combination', (done) => {
-      chai
-        .request(app)
-        .post('/auth')
-        .send(authJSON)
-        .end((err, res) => {
-          const data = res.body.data;
-
-          assert.equal(res.status, 401, 'Wrong status code');
-          assert.equal(res.body.code, 1002, 'Wrong error code');
-          assert.isString(res.body.message, 'No error message');
-
-          done();
-        });
-    });
-  });
+               done();
+            });
+      });
+   });
 });
