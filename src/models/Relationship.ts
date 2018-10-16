dfe781f1a5d8405352a227a8478c9b0a11ba4783
Dimas Gabriel <dimas@dimasgabriel.net>
2018-10-15 23:01:48 -0300

    Relationship model

--- src/models/Relationship.ts
+++ src/models/Relationship.ts
@@ -1,7 +1,33 @@
 import {Document, Schema, Model, model} from 'mongoose';
 
-export interface IRelationshipModel {
+export interface RelationshipModel extends Document {
    fromUser: Schema.Types.ObjectId;
    toUser: Schema.Types.ObjectId;
    relation: String;
 }
+
+const RelationshipSchema = new Schema({
+   fromUser: {
+      type: Schema.Types.ObjectId,
+      ref: 'User',
+      required: true
+   },
+
+   toUser: {
+      type: Schema.Types.ObjectId,
+      ref: 'User',
+      required: true
+   },
+
+   relation: {
+      type: String,
+      enum: ['follow', 'block']
+   }
+});
+
+RelationshipSchema.index({fromUser: 1, toUser: 1}, {unique: true});
+
+export const Relationship: Model<RelationshipModel> = model<RelationshipModel>(
+   'Relationship',
+   RelationshipSchema
+);
